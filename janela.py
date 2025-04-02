803a9b1badb8a2f4428981d50e7a5d17e722e6de
gusta437 <gc3265643@gmail.com>
2025-04-01 21:52:36 -0300

    Add files via upload

---
+++ janela.py
@@ -0,0 +1,64 @@
+import tkinter 
+
+root = tkinter.Tk()
+root.title("Hell World grafico")
+root.geometry("800x600")
+
+labelFrase = tkinter.Label(root,
+                           text="Olâ progamador",
+                           font=("chiller" , 100),
+                           fg='magenta',)
+                           
+labelFrase.pack (padx=5,pady=5)
+
+
+labelFrase = tkinter.Label(root,
+                           text='digite seu nome')
+labelFrase.pack(padx=5,pady=5)
+
+entryNome = tkinter.Entry(root, width=100)
+entryNome.pack(padx=99, pady=5)
+
+buttonGravar = tkinter.Button(root,
+                        text='gravar',
+                        commando=None )
+
+
+buttonGravar.pack(padx=10, pady=10)
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
